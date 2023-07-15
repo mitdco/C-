4e5c5af102f89f9c9d80c44ac9365366266fbedb
SmaJafari <jafari.sma@gmail.com>
2023-07-15 16:01:25 +0330

    hello World

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int a = 10;
+                long b = 3147483648;
+           // a = (int) b;
+             a = ((int)b);
+
+            Console.WriteLine(a.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
